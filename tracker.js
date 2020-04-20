0df194bbd6a2547b09ee81d702e107696c18a917
Akash Pillai <akashp807@gmail.com>
2020-04-19 21:30:10 -0500

    Generate data table using given values

--- tracker.js
+++ tracker.js
@@ -2,6 +2,7 @@
 
 // App globals
 let countriesToTrack = [];
+let chosenStatistic = '';
 let countryData = {};
 
 
@@ -25,9 +26,9 @@ const createCountryListItem = countryName => {
     graphic.appendChild(deleteButton);
 
     // Add event listener for deleting country list item
-    deleteButton.addEventListener('click', (e) => {
+    deleteButton.addEventListener('click', e => {
         // Get country text
-        const countryToDelete = e.target.parentNode.parentNode.childNodes[0].textContent;
+        const countryToDelete = e.target.parentNode.parentNode.childNodes[1].textContent;
 
         // Filter out country from global array and remove DOM node
         countriesToTrack = countriesToTrack.filter(country => country !== countryToDelete);
@@ -85,4 +86,94 @@ countryAddButton.addEventListener('click', () => {
 
     // Reset country input field
     document.querySelector('#country-input').value = '';
+});
+
+const createChartButton = document.querySelector('#create-chart');
+createChartButton.addEventListener('click', () => {
+    const table = document.querySelector('table');
+
+    // Reset table if it already exists
+    if (!!document.querySelector('thead')) {
+        table.removeChild(document.querySelector('thead'));
+        table.removeChild(document.querySelector('tbody'));
+    }
+
+    // Break out if nothing's been inputted
+    if (countriesToTrack.length === 0 || chosenStatistic === '') return;
+
+    // Create table head
+    const tableHead = document.createElement('thead');
+    const tableHeadRow = document.createElement('tr');
+    tableHeadRow.classList.add('mdc-data-table__header-row');
+    tableHead.appendChild(tableHeadRow);
+    table.appendChild(tableHead);
+
+    // Create date header
+    const dateHeader = document.createElement('th');
+    dateHeader.classList.add('mdc-data-table__header-cell');
+    dateHeader.setAttribute('role', 'columnheader');
+    dateHeader.setAttribute('scope', 'col');
+    dateHeader.textContent = 'Date';
+    tableHeadRow.appendChild(dateHeader);
+
+    // Create headers for each country
+    countriesToTrack.forEach(country => {
+        const newColumn = document.createElement('th');
+        newColumn.classList.add('mdc-data-table__header-cell');
+        newColumn.setAttribute('role', 'columnheader');
+        newColumn.setAttribute('scope', 'col');
+        newColumn.textContent = country;
+
+        tableHeadRow.appendChild(newColumn);
+    });
+
+    // Create table body
+    const tableBody = document.createElement('tbody');
+    table.appendChild(tableBody);
+
+    // Populate table with data
+    const datapoints = countryData[countriesToTrack[0]].length;
+
+    for (let i = 0; i < datapoints; i++) {
+        // Create table row
+        const row = document.createElement('tr');
+        row.classList.add('mdc-data-table__row');
+
+        // Add date
+        const date = document.createElement('td');
+        date.classList.add('mdc-data-table__cell');
+        date.textContent = countryData.Afghanistan[i].date;
+        row.appendChild(date);
+
+        // Create data nodes for each country
+        countriesToTrack.forEach(country => {
+            // Chosen statistic
+            const stat = document.createElement('td');
+            stat.classList.add('mdc-data-table__cell');
+            
+            stat.textContent = countryData[country][i][chosenStatistic];
+
+            row.append(stat);
+            tableBody.appendChild(row);
+        });
+    }
+    
+    // Set table view header according to chosen statistic
+    let tableViewHeader = 'Chart of ';
+    switch (chosenStatistic) {
+        case 'confirmed':
+            tableViewHeader += 'confirmed cases';
+            break;
+        case 'deaths':
+            tableViewHeader += 'deaths';
+            break;
+        case 'recovered':
+            tableViewHeader += 'recovered cases';
+            break;
+    }
+    tableViewHeader += ' over time';
+    document.querySelector('h1#chart-header').textContent = tableViewHeader;
+
+    // Navigate to table screen
+    document.querySelector('a[href="#table"]').click();                 
 });
